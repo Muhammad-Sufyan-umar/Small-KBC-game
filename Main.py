1333d00d34022d2c78a4786ab946f2566418f6cd
Muhammad-Sufyan-umar <sufyanumer04@gmail.com>
2026-08-12 14:52:53 +0500

    Refactor variable names for clarity and consistency in KBC game logic

--- Main.py
+++ Main.py
@@ -1,36 +1,36 @@
-a=input("What is your name: ")
-print("welcome sir ",a,"may wish you won:")
+user=input("What is your name: ")
+print("welcome sir ",user,"may wish you won:")
 
 
-b=["Who was the first Mughal emperor in India?:","Who win the T20 Wc of 2009","total number of planet on solar system:","which planet is known as Red Planet."]
-c=["babur","pakistan","8","mars"]
+Questions=["Who was the first Mughal emperor in India?:","Who win the T20 Wc of 2009","total number of planet on solar system:","which planet is known as Red Planet."]
+Ans=["babur","pakistan","8","mars"]
 
-g=100000
-h=200000
-j=300000
-k=400000000
+Lv1=100000
+Lv2=200000
+Lv3=300000
+Lv4=400000000
 
-a1=input(b[0])
+a1=input(Questions[0])
 
-if a1.lower()==c[0]:
-    print("Correct answer:you have won Rs.",g)
+if a1.lower()==Ans[0]:
+    print("Correct answer:you have won Rs.",Lv1)
     
 
-    a2=input(b[1])
-    if a2.lower()==c[1]:
-        print("Correct answer you have won Rs.",h)
+    a2=input(Questions[1])
+    if a2.lower()==Ans[1]:
+        print("Correct answer you have won Rs.",Lv2)
 
-        a3=input(b[2])
-        if a3.lower()==c[2]:
-            print("Correct you have won Rs.",j)
-            a4=input(b[3])
-            if a4.lower()==c[3]:
-                print("Correct you have win Bumper price '7 crore'",k)
+        a3=input(Questions[2])
+        if a3.lower()==Ans[2]:
+            print("Correct you have won Rs.",Lv3)
+            a4=input(Questions[3])
+            if a4.lower()==Ans[3]:
+                print("Correct you have win Bumper price '7 crore'",Lv4)
             else:
-                print("wrong answer, you can take money",k)
+                print("wrong answer, you can take money",Lv4)
         else:
-            print("wrong answer: you can take money",j)
+            print("wrong answer: you can take money",Lv3)
     else:
-        print("wrong answer, you can take money ",h)
-else:
+        print("wrong answer, you can take money ",Lv2)
+else:   
     print("tumse na ho payegaa baccha:" )
