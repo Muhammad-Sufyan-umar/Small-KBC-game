e0cefd7f766f9b814041b915ac8f9cec28440655
Muhammad-Sufyan-umar <sufyanumer04@gmail.com>
2026-08-12 16:59:44 +0500

    Improve user interaction in KBC game by enhancing prompts and formatting for clarity

--- Main.py
+++ Main.py
@@ -3,8 +3,10 @@ class KBC:
     def __init__(self):
         print("=======Welcome to KBC Game========")
     def start(self):
+        print("\n")
         user=input("What is your Name Sir: ")
-        print(f"welcome sir {user} may wish you will win:")
+        print("\n")
+        print(f"welcome sir {user} may wish you will win: \n")
 
 
         Questions=["Who was the first Mughal emperor in India?:","Who win the T20 Wc of 2009?","total number of planet on solar system?:","which planet is known as Red Planet?","Which day is observed as world environment day?","Which is the largest continent in the world?","Which is the largest ocean in the world?","Which is the largest desert in the world?","Which is the largest river in the world?","Which is the largest country in the world?"]
@@ -24,62 +26,62 @@ class KBC:
         Q1=input(Questions[0]+"\n1: Akbar \n2: Babur \n3: Humayun \n4: Shahjahan\n Ans: ")
 
         if Q1.lower()==Ans[0]:
-            print("Correct answer:you have won Rs.",Lv1)
+            print("\n 🎉Correct answer:you have won Rs.",Lv1,"\n")
     
 
             Q2=input(Questions[1]+"\n1: India \n2: Pakistan \n3: Bangladesh \n4: Sri Lanka\n Ans: ")
 
             if Q2.lower()==Ans[1]:
 
-                print("Correct answer you have won Rs.",Lv2)
+                print("\n 🎉Correct answer you have won Rs.",Lv2,"\n")
 
                 Q3=input(Questions[2]+"\n1: 7 \n2: 8 \n3: 9 \n4: 10\n Ans: ")
 
                 if Q3.lower()==Ans[2]:
 
-                    print("Correct you have won Rs.",Lv3)
+                    print("\n 🎉Correct you have won Rs.",Lv3,"\n")
 
                     Q4=input(Questions[3]+"\n1: Venus \n2: Mars \n3: Jupiter \n4: Saturn\n Ans: ")
 
                     if Q4.lower()==Ans[3]:
 
-                        print("Correct you have won price: ",Lv4)
+                        print("\n 🎉Correct you have won price: ",Lv4,"\n")
 
                         Q5=input(Questions[4]+"\n1: 5 June \n2: 6 June \n3: 7 June \n4: 8 June\n Ans: ")
 
                         if Q5.lower()==Ans[4]:
 
-                            print("Correct you have won price: ",Lv5)
+                            print("\n 🎉Correct you have won price: ",Lv5,"\n")
 
                             Q6=input(Questions[5]+"\n1: Asia \n2: Africa \n3: Europe \n4: Australia\n Ans: ")
 
                             if Q6.lower()==Ans[5]:
 
-                                print("Correct you have won price: ",Lv6)
+                                print("\n 🎉Correct you have won price: ",Lv6,"\n")
 
                                 Q7=input(Questions[6]+"\n1: Atlantic Ocean \n2: Indian Ocean \n3: Pacific Ocean \n4: Arctic Ocean\n Ans: ")
 
                                 if Q7.lower()==Ans[6]:
 
-                                    print("Correct you have won price: ",Lv7)
+                                    print("\n 🎉Correct you have won price: ",Lv7,"\n")
 
                                     Q8=input(Questions[7]+"\n1: Gobi Desert \n2: Kalahari Desert \n3: Sahara Desert \n4: Arabian Desert\n Ans: ")
 
                                     if Q8.lower()==Ans[7]:
 
-                                        print("Correct you have won price: ",Lv8)
+                                        print("\n 🎉Correct you have won price: ",Lv8,"\n")
 
                                         Q9=input(Questions[8]+"\n1: Amazon River \n2: Nile River \n3: Yangtze River \n4: Mississippi River\n Ans: ")
 
                                         if Q9.lower()==Ans[8]:
 
-                                            print("Correct you have won price: ",Lv9)
+                                            print("\n 🎉Correct you have won price: ",Lv9,"\n")
 
                                             Q10=input(Questions[9]+"\n1: China \n2: USA \n3: Russia \n4: Canada\n Ans: ")
 
                                             if Q10.lower()==Ans[9]:
 
-                                                print("Correct you have won price:",Lv10)
+                                                print("\n 🎉Correct you have won price:",Lv10,"\n")
 
                                             else:
 
